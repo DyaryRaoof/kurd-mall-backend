5cf73408229bfeae4885dfffc5370f3cc7396db4
DyaryRaoof <dyary2020@gmail.com>
2022-02-19 14:55:47 +0300

    Add bootstrap styling to devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
     def update_allowed_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :image, :phone, :is_driver, :is_admin) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :image, :phone, :is_driver, :is_admin) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :image, :phone, :is_driver, :is_admin) }
     end
 
     def after_sign_out_path_for(resource_or_scope)
