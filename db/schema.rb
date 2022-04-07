01501a09ffeb10eeff9cf47f35f3cee669f079e2
DyaryRaoof <dyary2020@gmail.com>
2022-04-07 21:10:54 +0300

    Migrate users phone to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_210032) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_180050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -281,7 +281,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_30_210032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.bigint "phone"
+    t.string "phone"
     t.boolean "is_driver"
     t.boolean "is_admin"
     t.string "confirmation_token"
